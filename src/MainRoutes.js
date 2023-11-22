58c7391788118b0426bf145e99a0d99680a68076
CRRodri <144954672+CRRodri@users.noreply.github.com>
2023-11-22 13:58:49 -0600

    Add files via upload

---
+++ src/MainRoutes.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import MainPg from "./MainPg";
+import Signin from "./Signin";
+import Signup from "./Signup";
+import Visitorpage from "./Visitorpage";
+import Maintenance from "./Maintenance";
+
+function MainRoutes() {
+  return (
+    <BrowserRouter>
+        <Routes> 
+          <Route path="/" element={<MainPg />} />
+          <Route path="/Signin" element={<Signin />} /> 
+          <Route path="/Signup" element={<Signup />} /> 
+          <Route path="/Visitorpage" element={<Visitorpage />} />
+          <Route path="/Maintenance" element={<Maintenance />} />
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default MainRoutes;
